5befa4ec39e081218c25904d2d8699a52ebd0fa1
Usuario <Usuario@192.168.130.40>
2016-07-29 10:02:17 -0500

    armas

---
+++ BoGoDark-V2/src/Armas/cuchillo.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Armas;
+
+/**
+ *
+ * @author Usuario
+ */
+public class cuchillo {
+    
+    private String nombre;
+    private int damage;
+    private int alcanze;
+    private int velocidad;
+    
+     public cuchillo(){
+     this.nombre="";
+     this.damage=10;
+     this.alcanze=5;
+     this.velocidad=30;
+     }
+    
+}
