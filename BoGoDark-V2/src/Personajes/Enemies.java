0aced7ee2c7329c2f8f95978740dd4427360453b
Usuario <Usuario@192.168.130.33>
2016-08-04 08:16:33 -0500

    TD4W!!

---
+++ BoGoDark-V2/src/Personajes/Enemies.java
@@ -0,0 +1,135 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Personajes;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Enemies {
+    String name;
+    int life;
+    int damage;
+    int defense;
+    int level;
+    int exp;
+    int strong;//Esto aun no se usa 
+    int intelligence;//Esto aun no se usa
+    int dexterity;//Esto aun no se usa
+    int avoid;///Esto aun no se usa
+    int critical;//Esto aun no se usa
+    boolean turno;
+    
+    
+    public Enemies(String name)
+    {
+    this.name = name;
+    this.life = 150;
+    this.damage = 20;
+    this.defense = 40;
+    this.level = 1;
+    this.exp = 20;
+    this.turno = false;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getLife() {
+        return life;
+    }
+
+    public void setLife(int life) {
+        this.life = life;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+
+    public void setDamage(int damage) {
+        this.damage = damage;
+    }
+
+    public int getDefense() {
+        return defense;
+    }
+
+    public void setDefense(int defense) {
+        this.defense = defense;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public int getExp() {
+        return exp;
+    }
+
+    public void setExp(int exp) {
+        this.exp = exp;
+    }
+
+    public int getStrong() {
+        return strong;
+    }
+
+    public void setStrong(int strong) {
+        this.strong = strong;
+    }
+
+    public int getIntelligence() {
+        return intelligence;
+    }
+
+    public void setIntelligence(int intelligence) {
+        this.intelligence = intelligence;
+    }
+
+    public int getDexterity() {
+        return dexterity;
+    }
+
+    public void setDexterity(int dexterity) {
+        this.dexterity = dexterity;
+    }
+
+    public int getAvoid() {
+        return avoid;
+    }
+
+    public void setAvoid(int avoid) {
+        this.avoid = avoid;
+    }
+
+    public int getCritical() {
+        return critical;
+    }
+
+    public void setCritical(int critical) {
+        this.critical = critical;
+    }
+
+    public boolean isTurno() {
+        return turno;
+    }
+
+    public void setTurno(boolean turno) {
+        this.turno = turno;
+    }
+    
+    
+}
